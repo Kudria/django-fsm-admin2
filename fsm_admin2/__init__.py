5bfdecd196ab71a72bf0b96b80713cce79c8b206
kudria <kudria15@gmail.com>
2020-10-08 16:08:18 +0300

    Add app config

--- fsm_admin2/__init__.py
+++ fsm_admin2/__init__.py
@@ -1,1 +1,1 @@
-from .admin import FSMTransitionMixin
+default_app_config ='fsm_admin2.apps.FsmAdmin2Config'
---
+++ fsm_admin2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FsmAdmin2Config(AppConfig):
+    name = __package__

d75192d4e9b3e26681aee6921b5609ea60a7d43a
kudria <kudria15@gmail.com>
2020-10-23 17:32:10 +0300

    Add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import os
+
 from setuptools import setup
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django-fsm-admin2',
     version='0.1.1',
     packages=['fsm_admin2'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     url='https://github.com/Kudria/django-fsm-admin2',
     author='Alexandr Kudriavtcev',
